2aca9bb0727d1399e7e6f4ffc209b20e7ca1576f
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-11-08 22:34:36 +0600

    firebase .env config

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -1,18 +1,33 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDHsvzyBMlwe1A5BYd7FLeBt6I2BzZMOi0",
+//   authDomain: "zero-dollar-bites.firebaseapp.com",
+//   projectId: "zero-dollar-bites",
+//   storageBucket: "zero-dollar-bites.appspot.com",
+//   messagingSenderId: "17022929787",
+//   appId: "1:17022929787:web:5bd23f6b0bc0f127038bde"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export default app;
+
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDHsvzyBMlwe1A5BYd7FLeBt6I2BzZMOi0",
-  authDomain: "zero-dollar-bites.firebaseapp.com",
-  projectId: "zero-dollar-bites",
-  storageBucket: "zero-dollar-bites.appspot.com",
-  messagingSenderId: "17022929787",
-  appId: "1:17022929787:web:5bd23f6b0bc0f127038bde"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
