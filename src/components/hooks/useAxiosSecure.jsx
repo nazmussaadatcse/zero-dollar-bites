f06356ee42a6a622faaf9c78d2c94ff70c421e63
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-11-08 22:13:23 +0600

    useAxiosSecure

---
+++ src/components/hooks/useAxiosSecure.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { AuthContext } from "./AuthProvider";
+import { useNavigate } from "react-router-dom";
+
+
+const axiosSecure = axios.create({
+    baseURL: 'http://localhost:5000',
+    withCredentials: true
+})
+
+const useAxiosSecure = () => {
+    const { logOut } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        axiosSecure.interceptors.response.use(res => {
+            return res;
+        }, error => {
+            console.log('interceptor error:', error.response);
+            if (error.response.status === 401) {
+                logOut()
+                    .then(() => {
+                        navigate('/login')
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            }
+        })
+    }, []);
+
+    return axiosSecure;
+};
+
+export default useAxiosSecure;
