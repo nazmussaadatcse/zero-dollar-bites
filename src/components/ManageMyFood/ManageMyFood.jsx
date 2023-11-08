4ff3a7bbac0ce4658f62c664489487c23cf7cae7
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-11-08 14:50:08 +0600

    react table and delete from table succeed

--- src/components/ManageMyFood/ManageMyFood.jsx
+++ src/components/ManageMyFood/ManageMyFood.jsx
@@ -2,7 +2,8 @@ import { AuthContext } from "../AuthProvider";
 import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
-
+import React from 'react';
+import { useTable } from 'react-table';
 
 
 const ManageMyFood = () => {
@@ -11,6 +12,7 @@ const ManageMyFood = () => {
     const [foods, setFoods] = useState([]);
     console.log(user?.email);
     console.log(foods);
+    const data = foods;
 
     useEffect(() => {
         fetch(`http://localhost:5000/food`)
@@ -22,8 +24,9 @@ const ManageMyFood = () => {
             })
     }, [user]);
 
-    const handelDelete = (id) => {
-        console.log(id);
+    const handleDelete = (id) => {
+        console.log('handleDelete', id);
+        const email = user.email;
 
         Swal.fire({
             title: 'Are you sure?',
@@ -36,7 +39,7 @@ const ManageMyFood = () => {
         }).then((result) => {
             if (result.isConfirmed) {
 
-                fetch(`http://localhost:5000/food/${id}`, {
+                fetch(`http://localhost:5000/food/${id}?email=${email}`, {
                     method: 'DELETE'
                 })
                     .then(res => res.json())
@@ -60,43 +63,108 @@ const ManageMyFood = () => {
 
     }
 
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Item',
+                accessor: (row, i) => i + 1,
+            },
+            {
+                Header: 'Name',
+                accessor: 'name',
+            },
+            {
+                Header: 'Status',
+                accessor: 'foodStatus',
+            },
+            {
+                Header: 'Quantity',
+                accessor: 'quantity',
+            },
+            {
+                Header: 'Expire on',
+                accessor: 'expiry_date',
+            },
+            {
+                Header: 'Update',
+                accessor: (row) => (
+                    <Link to={`/manage/${row._id}`}>
+                        <button className="btn btn-xs mt-6 m-1 btn-outline rounded-md">manage</button>
+                    </Link>
+                ),
+            },
+            {
+                Header: 'Manage',
+                accessor: (row) => (
+                    <Link to={`/update/${row._id}`}>
+                        <button className="btn btn-xs mt-6 m-1 btn-outline rounded-md">update</button>
+                    </Link>
+                ),
+            },
 
+            {
+                Header: 'Delete',
+                accessor: (row) => (
+                    <button
+                        onClick={() => handleDelete(row._id)}
+                        className="btn btn-xs mt-6 m-1 btn-outline rounded-md"
+                    >
+                        Delete
+                    </button>
+                ),
+            },
+
+
+        ],
+        []
+    );
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({
+        columns,
+        data,
+    });
 
 
     return (
-        <div>
-            <h2>Manage My Food</h2>
-            <div>
-            {
-                foods.map((food, index) => <div key={food._id} className="block md:flex p-2 bg-slate-200 m-2 gap-2">
-                    <p className="flex justify-center items-center text-center bg-slate-50 p-1 text-blue-800 text-xs font-bold">Item {index + 1}</p>
-                    <div className="md:w-1/2 rounded-lg">
-                        <p className="text-purple-700 gap-1"><span className="font-bold">Name:</span> {food?.name}</p>
-                        <p className="text-purple-700 gap-1"><span className="font-bold">Status:</span> {food?.foodStatus}</p>
-                    </div>
-                    <div className="md:w-1/2 rounded-lg">
-                        <p className="text-purple-700 gap-1"><span className="font-bold">Quantity:</span> ${food?.quantity}</p>
-                        <p className="text-purple-700 gap-1"><span className="font-bold">Expire on:</span> {food?.expiry_date}</p>
-                    </div>
-                    
-                    
-                    <Link to={"/manage/:id"}>
-                    <button onClick={() => handelDelete(food._id)} className=" btn btn-xs mt-6 m-1 btn-outline rounded-md">
-                        update
-                    </button>
-                    </Link>
-                    <button onClick={() => handelDelete(food._id)} className=" btn btn-xs mt-6 m-1 btn-outline rounded-md">
-                        manage
-                    </button>
-                    <Link to={"/update/:id"}>
-                    <button onClick={() => handelDelete(food._id)} className=" btn btn-xs mt-6 m-1 btn-outline rounded-md">
-                        manage
-                    </button>
-                    </Link>
-                </div>)
-            }
+
+        <div className="p-8">
+            <div className="flex justify-center p-4">
+                <h2 className="text-xl font-semibold text-green-700">Manage My Food <hr className="p-1 bg-green-700" /> </h2>
             </div>
+            <table {...getTableProps()} className="table">
+                <thead>
+                    {headerGroups.map((headerGroup, i) => (
+                        <tr key={i} {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column, j) => (
+                                <th key={j} {...column.getHeaderProps()}>{column.render('Header')}</th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody
+
+                    {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()} key={row.original._id}>
+                                {row.cells.map((cell, k) => (
+                                    <td key={k} {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                ))}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
+
+
     );
 };
 
